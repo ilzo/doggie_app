27fbfe459ac15b2114b2958770f20c760423d98f
Ilari Juvani <ilari.juvani@metropolia.fi>
2016-11-23 20:39:47 +0200

    linked events data is now output to the events page

--- web/js/52dbcd8_linked-events-data_1.js
+++ web/js/52dbcd8_linked-events-data_1.js
@@ -2,7 +2,16 @@ var linkedEventsData1;
 var linkedEventsData2;
 var linkedEventsResults1 = [];
 var linkedEventsResults2 = [];
-
+var allEventResults = [];
+var eventObject;
+var formattedEventObjects = [];
+var eventName;
+var eventDescShort;
+var eventDescLong;
+var shortenThis;
+var eventStartTimes = [];
+var eventEndTimes = [];
+var eventImages = [];
 
 
 var linkedEventsRequest1 = $.ajax({
@@ -27,16 +36,7 @@ $.when(linkedEventsRequest1, linkedEventsRequest2).done(function(data1, data2) {
     $.each( linkedEventsData1[0]['data'], function( key, value ) {
         linkedEventsResults1.push(value);
     });
-    
-    console.log("linkedEventsResults1:");  
-    console.log(linkedEventsResults1);
-    linkedEventsResults1.sort(compare);
-      
-    var containerArr1 = sortRecurrentEvents(linkedEventsResults1);
-      
-    console.log("Container array 1:");
-    console.log(containerArr1);
-    
+
   }
     
     
@@ -45,19 +45,41 @@ $.when(linkedEventsRequest1, linkedEventsRequest2).done(function(data1, data2) {
     $.each( linkedEventsData2[0]['data'], function( key, value ) {
         linkedEventsResults2.push(value);
     });
-    console.log("linkedEventsResults2:");  
-    console.log(linkedEventsResults2);
-    linkedEventsResults2.sort(compare);
-      
-    var containerArr2 = sortRecurrentEvents(linkedEventsResults2);
-      
-    console.log("Container array 2:");
-    console.log(containerArr2);
+     
     
   }
+    
+    console.log("linkedEventsResults1:");  
+    console.log(linkedEventsResults1);
+    
+    console.log("linkedEventsResults2:");  
+    console.log(linkedEventsResults2);
+    
+    
+    allEventResults = linkedEventsResults1.concat(linkedEventsResults2);
+    
+    allEventResults.sort(compare);
+    
+    console.log("calling duplicate removal:");
+    var uniqueArray = removeDuplicates(allEventResults, "id");
+    
+    console.log("event results: ");  
+    console.log(uniqueArray);
+    
+    
+    var containerArr = sortRecurrentEvents(uniqueArray);
+    
+    console.log("Container array:");
+    console.log(containerArr);
+    
+    formatEventData(containerArr);
+    
+    formattedEventObjects.reverse();
+    console.log("formatted events:");
+    console.log(formattedEventObjects);
+    
+    outputEvents(formattedEventObjects);
   
-    
-    
 });
 
 linkedEventsRequest1.fail(function( jqXHR, textStatus ) {
@@ -67,7 +89,6 @@ linkedEventsRequest1.fail(function( jqXHR, textStatus ) {
 linkedEventsRequest2.fail(function( jqXHR, textStatus ) {
   console.log( "Request failed: " + textStatus );
 });
-
 
 
 
@@ -109,11 +130,19 @@ function sortRecurrentEvents (eventsArr) {
                 return false;
                 
             }else{
+                
+                
                 if(val.description.fi !== comparedItem.description.fi) {
-                    var subArr = eventsArr.slice(i, pointer);
-                    resultArr[j] = subArr;
-                    j++;
-                    flag = true;
+                    
+                    if(val.name.fi !== comparedItem.name.fi) {
+                        
+                        var subArr = eventsArr.slice(i, pointer);
+                        resultArr[j] = subArr;
+                        j++;
+                        flag = true;
+                        
+                    }
+                      
                 }
                 
             }
@@ -127,9 +156,138 @@ function sortRecurrentEvents (eventsArr) {
 }
 
 
+function formatEventData (container) {
+    $.each( container, function( key, value ) {
+        if(value.length == 1){
+            
+            if (value[0].images.length > 0) {
+                for(var i = 0; i < value[0].images.length; i++) {
+                    eventImages.push(value[0].images[i].url);
+                }
+                
+            }else{
+                eventImages.push("../img/event4.jpg");
+            }
+            
+            if(value[0].short_description !== null) {
+                eventDescShort = value[0].short_description.fi;
+            }else{
+                shortenThis = value[0].description.fi;
+                eventDescShort = shortenThis.substring(0, 80) + "...";
+            }
+            
+            eventObject = {name : value[0].name.fi, start : reformatTime(value[0].start_time), end : reformatTime(value[0].end_time), desc_short : eventDescShort, desc_long : value[0].description.fi, images : eventImages};
+            formattedEventObjects.push(eventObject);
+            
+        }else{
+            
+            eventName = value[0].name.fi;
+            
+            if(value[0].short_description !== null) {
+                eventDescShort = value[0].short_description.fi;
+            }else{
+                shortenThis = value[0].description.fi;
+                eventDescShort = shortenThis.substring(0, 80) + "...";
+            }
+            
+            eventDescLong = value[0].description.fi;
+            
+            if (value[0].images.length > 0) {
+                for(var i = 0; i < value[0].images.length; i++) {
+                    eventImages.push(value[0].images[i].url);
+                }
+                
+            }else{
+                eventImages.push("../img/event4.jpg");
+            }
+            
+            
+            for(var i = 0; i < value.length; i++) {
+                
+                eventStartTimes.push(reformatTime(value[i].start_time));
+                
+                eventEndTimes.push(reformatTime(value[i].end_time));
+                
+                
+            }
+            
+            eventObject = {name : eventName, start : eventStartTimes, end : eventEndTimes, desc_short : eventDescShort, desc_long : eventDescLong, images : eventImages};
+            formattedEventObjects.push(eventObject);
+            
+            eventStartTimes = [];
+            eventEndTimes = [];
+            
+        }
+        
+        eventImages = [];
+        
+    });
+     
+}
 
+function reformatTime (string) {
+    
+    if(string == null) {
+        return null;
+    }
+    
+    if ((string.indexOf("T") !== -1) && (string.indexOf("Z") !== -1)){
+        var dateString = string.substring(0,string.lastIndexOf("T"));
+        var dateArr = dateString.split("-",3);
+        dateString = dateArr[2] + "." + dateArr[1] + "." + dateArr[0];
+        var timeString = string.substring(string.lastIndexOf("T")+1, 16);
+    }else{
+        var dateArr = string.split("-",3);
+        var dateString = dateArr[2] + "." + dateArr[1] + "." + dateArr[0];
+        var timeString = null;
+    }
+    
+    var dateObj = {date : dateString , time : timeString};
+    return dateObj;
+    
+}
 
-/*
+function outputEvents(events) {
+    var eventDate;
+    var itemCount = 0;
+    var row = $('<div class="row"></div>');
+    var item;
+    
+    for(var i = 0; i < events.length; i++) {
+        
+        if(i > 0) {
+            if(itemCount == 0){
+                $(".content-area .container").append(row);
+                row = $('<div class="row"></div>');
+
+            }   
+        }
+        
+        if( Object.prototype.toString.call( events[i].start ) === '[object Array]' ) {
+            eventDate = events[i].start[0].date;
+        }else{
+            eventDate = events[i].start.date;
+        }
+        
+        item = $('<div id="'+i+'" class="col-md-3 event-item"><a href="#"><img class="img-responsive" src="'+events[i].images[0]+'"></a><h3 class="name-of-the-event"><a href="#">'+events[i].name+'</a></h3><p>'+eventDate+'</p><p>'+events[i].desc_short+'</p></div>');
+        
+        row.append(item);
+        
+        if(events.length - i == 1) {
+                $(".content-area .container").append(row);
+                break;
+        }
+        
+        if(itemCount < 3) {
+            itemCount++; 
+        }else{
+            itemCount = 0;
+        }
+            
+          
+    }
+      
+}
 
 
 function removeDuplicates(arr, prop) {
@@ -146,60 +304,14 @@ function removeDuplicates(arr, prop) {
  
      return new_arr;
  }
- */
  
 
-
-
-/*
-function checkDuplicateInObject(propertyName, inputArray) {
-  //var seenDuplicate = false;
-  var testObject = {};
- 
-  inputArray.map(function(item) {
-    var itemPropertyName = item[propertyName];    
-    if (itemPropertyName in testObject) {
-      testObject[itemPropertyName].duplicate = true;
-      item.duplicate = true;
-      duplicates.push(item);
-      //seenDuplicate = true;
-        console.log("ItemPropertyName:");
-        console.log(itemPropertyName);
-        
-    }else{
-      testObject[itemPropertyName] = item;
-        
-      delete item.duplicate;
-    }
-  });
- 
-  return duplicates;
-}
-*/
-
-
-/*
-linkedEventsRequest.done(function(result) {
-  linkedEventsData = result;
-    
-  if( linkedEventsData ) {
-    $.each( linkedEventsData, function( key, value ) {
-        linkedEventsResults.push(value);
+function checkDuplicates(arr) {
+    var valueArr = arr.map(function(item){ return item.id });
+    var isDuplicate = valueArr.some(function(item, idx){ 
+        return valueArr.indexOf(item); 
     });
-    console.log("linkedEventsResults:");  
-    console.log(linkedEventsResults);  
-  }
     
-});
+    return isDuplicate;
+}
 
-
-
-
-var promise1 = $.ajax("/myServerScript1");
-var promise2 = $.ajax("/myServerScript2");
- 
-$.when(promise1, promise2).done(function(data1, data2) {
-  // Handle data from both Ajax calls
-});
-
-*/
